620127966fff762caa25b6ca1b9ad51212731221
Sumanth Chinthagunta <xmlking@gmail.com>
2019-10-15 19:19:36 -0700

    installing google-cloud-sdk via brew
    brew cask install google-cloud-sdk

--- home/my/extra.zsh
+++ home/my/extra.zsh
@@ -5,11 +5,12 @@
 ## move them here.
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f "/Users/schintha/Developer/Apps/google-cloud-sdk/path.zsh.inc" ]; then . "/Users/schintha/Developer/Apps/google-cloud-sdk/path.zsh.inc"; fi
+if [ -f "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]; then . "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f "/Users/schintha/Developer/Apps/google-cloud-sdk/completion.zsh.inc" ]; then . "/Users/schintha/Developer/Apps/google-cloud-sdk/completion.zsh.inc"; fi
+if [ -f "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]; then . "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"; fi
 
 # THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
